a30a40d261f2ee4ed462904ef3ba6056e3e4d695
MarianWhat <maria_zapata@hotmail.it>
2019-06-07 02:01:46 -0500

    Actualizacón para el 1er Sprint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const passVar = document.getElementById('pass');
 const msAlertaVar =document.getElementById('msAlerta');
-const contAccesoVar= document.getElementById('contAcceso');
+const contAcceso= document.getElementById('cont-acceso');
 const secInicio= document.getElementById('sec-inicio');
 const secDiario=document.getElementById('sec-diario');
 
 let intentos = 3;
+
+contAcceso.addEventListener('click',function () {
+	contAcceso.classList.add('click');
+	passVar.focus();
+});
 
 // Login
 passVar.addEventListener('keyup', function(event){
@@ -21,13 +26,12 @@ function validar(password) {
 		intentos--;
 		if (intentos>0) {
 		msAlertaVar.innerHTML=`Clave incorrecta, te quedan ${intentos} intento(s).`;
+		passVar.value='';
 	} else {
-		contAccesoVar.innerHTML=`<img src="img/icon-bloq.png">`;
+		contAcceso.innerHTML=`<img src="img/icon-bloq.png">`;
 		msAlertaVar.innerHTML=`Ya utilizaste todos tus intentos, en este momento no podrás ingresar`;
 		secInicio.classList.add("contBloquedo");
 
 	}
  }
 }
-
-//Cifraado
