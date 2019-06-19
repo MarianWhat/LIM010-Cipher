2857d4d436289bfa09e234033a0ca9747a296197
MarianWhat <maria_zapata@hotmail.it>
2019-06-19 10:16:12 -0500

    Actualización de test

--- src/cipher.js
+++ src/cipher.js
@@ -2,87 +2,98 @@ window.cipher = {
   encode: (offset, string) => {
     let cantCaracteres=string.length;
     let cadenaCifrada='';
+    let cifradoEnASCII;
+    const cifrar = (inicio,caracterASCII,campo) =>{
+      cifradoEnASCII=(caracterASCII-inicio+parseInt(offset))%campo+inicio;
+      if (cifradoEnASCII<inicio) {  
+          cifradoEnASCII=cifradoEnASCII+campo;
+        }
+      }
     for (var i = 0; i < cantCaracteres; i++) {
       let caracterASCII =string.charCodeAt(i);
-      let cifradoEnASCII;
-      if (caracterASCII >=32 && caracterASCII <=254) {
-         cifradoEnASCII=(caracterASCII-32+parseInt(offset))%222+32;
-          if (cifradoEnASCII<32) {  
-          cifradoEnASCII=cifradoEnASCII+222;
-          }
-        let caracterCifrado=String.fromCharCode(cifradoEnASCII);
-        cadenaCifrada+=caracterCifrado;
-        } else {
-         cadenaCifrada=cadenaCifrada+String.fromCharCode(caracterASCII);
-       }
-     }
+      //De 32 a 47
+      if (caracterASCII >=32 && caracterASCII <=47) {
+        cifrar(32,caracterASCII,16);
+      }
+      //De Numeros 48 a 57
+      if (caracterASCII >=48 && caracterASCII <=57) {
+        cifrar(48,caracterASCII,10);
+      }
+      //De signos 58 a 64
+      if (caracterASCII >=58 && caracterASCII <=64) {
+        cifrar(58,caracterASCII,7);
+      }
+      //De Letras Mayusculas 65 a 90
+      if (caracterASCII >=65 && caracterASCII <=90) {
+        cifrar(65,caracterASCII,26);
+      }
+      //De signos 91 a 96
+      if (caracterASCII >=91 && caracterASCII <=96) {
+        cifrar(91,caracterASCII,6);
+      }
+      //De Letras Minusculas 97 a 122
+      if (caracterASCII >=97 && caracterASCII <=122) {
+        cifrar(97,caracterASCII,26);
+      }
+      //De caracteres de 123 a 126
+      if (caracterASCII >=123 && caracterASCII <=126) {
+        cifrar(123,caracterASCII,4);
+      }
+      //De caracteres de 128 a 254
+      if (caracterASCII >=128 && caracterASCII <=254) {
+        cifrar(128,caracterASCII,127);
+      }
+      let caracterCifrado=String.fromCharCode(cifradoEnASCII);
+      cadenaCifrada+=caracterCifrado;
+    }
      return cadenaCifrada;
    },
    decode: (offset, string) => {
     let cantCaracteres=string.length;
     let cadenaDescifrada='';
-    let caracterDescifrado='';
+    let DescifradoEnASCII;
+    const decifrar = (inicio,caracterASCII,campo) =>{
+      DescifradoEnASCII=(caracterASCII-inicio-parseInt(offset))%campo+inicio;
+      if (DescifradoEnASCII<inicio) {  
+          DescifradoEnASCII=DescifradoEnASCII+campo;
+        }
+      }
     for (var i = 0; i < cantCaracteres; i++) {
       let caracterASCII =string.charCodeAt(i);
-      if (caracterASCII >=32 && caracterASCII <=254) {
-        caracterDescifrado=((caracterASCII-32-offset)%222+32);
-        if (caracterDescifrado<32) {          
-         caracterDescifrado=caracterDescifrado+222;
-         cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterDescifrado);
-       } else {
-       cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterDescifrado);
-       }
-      } else {
-       cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterASCII);
+      //De 32 a 47
+      if (caracterASCII >=32 && caracterASCII <=47) {
+        decifrar(32,caracterASCII,16);
       }
+      //De Numeros 48 a 57
+      if (caracterASCII >=48 && caracterASCII <=57) {
+        decifrar(48,caracterASCII,10);
+      }
+      //De signos 58 a 64
+      if (caracterASCII >=58 && caracterASCII <=64) {
+        decifrar(58,caracterASCII,7);
+      }
+      //De Letras Mayusculas 65 a 90
+      if (caracterASCII >=65 && caracterASCII <=90) {
+        decifrar(65,caracterASCII,26);
+      }
+      //De signos 91 a 96
+      if (caracterASCII >=91 && caracterASCII <=96) {
+        decifrar(91,caracterASCII,6);
+      }
+      //De Letras Minusculas 97 a 122
+      if (caracterASCII >=97 && caracterASCII <=122) {
+        decifrar(97,caracterASCII,26);
+      }
+      //De caracteres de 123 a 126
+      if (caracterASCII >=123 && caracterASCII <=126) {
+        decifrar(123,caracterASCII,4);
+      }
+      //De caracteres de 128 a 254
+      if (caracterASCII >=128 && caracterASCII <=254) {
+        decifrar(128,caracterASCII,127);
+      }
+     cadenaDescifrada=cadenaDescifrada+String.fromCharCode(DescifradoEnASCII);
     }
     return cadenaDescifrada;
   }
 };
-
-
-// CIPHER BASICO
-// window.cipher = {
-//   encode: (offset, string) => {
-//     let cantCaracteres=string.length;
-//     string = string.toUpperCase()
-//     let cadenaCifrada='';
-//     for (var i = 0; i < cantCaracteres; i++) {
-//       let caracterASCII =string.charCodeAt(i);
-//       let cifradoEnASCII;
-//       if (caracterASCII >=65 && caracterASCII <=90) {
-//          cifradoEnASCII=(caracterASCII-65+parseInt(offset))%26+65;
-//           if (cifradoEnASCII<65) {  
-//           cifradoEnASCII=cifradoEnASCII+26;
-//         }
-//         let caracterCifrado=String.fromCharCode(cifradoEnASCII);
-//         cadenaCifrada+=caracterCifrado;
-
-//         } else {
-//          cadenaCifrada=cadenaCifrada+String.fromCharCode(caracterASCII);
-//        }
-//      }
-//      return cadenaCifrada;
-//    },
-//    decode: (offset, string) => {
-//     let cantCaracteres=string.length;
-//     string = string.toUpperCase();
-//     let cadenaDescifrada='';
-//     let caracterDescifrado='';
-//     for (var i = 0; i < cantCaracteres; i++) {
-//       let caracterASCII =string.charCodeAt(i);
-//       if (caracterASCII >=65 && caracterASCII <=90) {
-//         caracterDescifrado=((caracterASCII-65-offset)%26+65);
-//         if (caracterDescifrado<65) {          
-//          caracterDescifrado=caracterDescifrado+26;
-//          cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterDescifrado);
-//        } else {
-//        cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterDescifrado);
-//        }
-//       } else {
-//        cadenaDescifrada=cadenaDescifrada+String.fromCharCode(caracterASCII);
-//       }
-//     }
-//     return cadenaDescifrada;
-//   }
-// };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -2,39 +2,44 @@ describe('cipher', () => {
   it('Debería ser un objeto.', () => {
     assert.equal(typeof cipher, 'object');
   });
-  //Para Hacker edition
   describe('cipher.encode', () => {
     it('Debería ser una función.', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
-    it('Debería retornar ">?@ABCDEFGHIJKLMNOPQRSTUVW" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset -3.', () =>{
-      assert.equal(cipher.encode(-3,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), '>?@ABCDEFGHIJKLMNOPQRSTUVW');
+    it('Debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33.', () =>{
+      assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+    it('Debería retornar "hijklmnopqrstuvwxyzabcedfg" para "abcdefghijklmnopqrstuvxwyz" con offset 33.', () =>{
+      assert.equal(cipher.encode(33,'abcdefghijklmnopqrstuvxwyz'), 'hijklmnopqrstuvwxyzabcedfg');
+    });
+    it('Debería retornar "3456789012" para "0123456789" con offset 33.', () =>{
+      assert.equal(cipher.encode(33,'0123456789'), '3456789012');
+    });
+    it('Debería retornar "| B,/ -`!É~[?" para "{/U+./,] ¨}^:" con offset 33.', () =>{
+      assert.equal(cipher.encode(33,'{/U+./,] ¨}^:'), '| B,/ -`!É~[?');
+    });
+    it('Debería retornar "tuv123456/`" para "abc456789 ]" con offset -33.', () =>{
+      assert.equal(cipher.encode(-33,'abc456789 ]'), 'tuv123456/`');
     });
   });
   describe('cipher.decode', () => {
     it('Debería ser una función.', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
-    it('Debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para ">?@ABCDEFGHIJKLMNOPQRSTUVW" con offset -3.',() =>{
-      assert.equal(cipher.decode(-3,'>?@ABCDEFGHIJKLMNOPQRSTUVW'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    it('Debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33.',() =>{
+      assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+    it('Debería retornar "abcdefghijklmnopqrstuvxwyz" para "hijklmnopqrstuvwxyzabcedfg" con offset 33.', () =>{
+      assert.equal(cipher.decode(33,'hijklmnopqrstuvwxyzabcedfg'), 'abcdefghijklmnopqrstuvxwyz');
+    });
+    it('Debería retornar "0123456789" para "3456789012" con offset 33.', () =>{
+      assert.equal(cipher.decode(33,'3456789012'), '0123456789');
+    });
+    it('Debería retornar "{/U+./,] ¨}^:" para "| B,/ -`!É~[?" con offset 33.', () =>{
+      assert.equal(cipher.decode(33,'| B,/ -`!É~[?'), '{/U+./,] ¨}^:');
+    });
+    it('Debería retornar "abc456789 ]" para "tuv123456/`" con offset -33.', () =>{
+      assert.equal(cipher.decode(-33,'tuv123456/`'), 'abc456789 ]');
     });
   });
-
-
-    //describe('cipher.encode', () => {
-  //   it('Debería ser una función.', () => {
-  //     assert.equal(typeof cipher.encode, 'function');
-  //   });
-  //   it('Debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33.', () =>{
-  //     assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
-  //   });
-  // });
-  // describe('cipher.decode', () => {
-  //   it('Debería ser una función.', () => {
-  //     assert.equal(typeof cipher.decode, 'function');
-  //   });
-  //   it('Debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33.',() =>{
-  //     assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-  //   });
-  // });
 })
